38005ca033839e99fee83d20019f864cb6562668
hangpham <myhang1503@gmail.com>
2025-05-04 22:39:03 +0700

    dxdxf

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-blue-500 text-white p-4 text-center font-poppins">
       <h1 class="text-2xl">Welcome to HP Macrame!</h1>
-      <p class="text-lg">Tailwind CSS is working aaaaaaa!</p>
+      <p class="text-lg">Tailwind CSS is workikljhjkghyuyhgung aaaaaaa!</p>
     </div>
   </template>
   
